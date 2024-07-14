1251c1be35c22b5a9a02e93d65165494921a5c61
MR-Gnawi <anime1111117@gmail.com>
2024-07-14 01:32:07 +0100

    Update Gnawi_v1.0.0.py

--- Gnawi_v1.0.0.py
+++ Gnawi_v1.0.0.py
@@ -1,19 +1,25 @@
 import requests
-import socket
+from termcolor import colored
+
+print(colored("-----------------------------------", 'green'))
+print(colored("|||         Gnawi_v1.0.0         |||", 'green'))
+print(colored("-----------------------------------", 'green'))
 
 def main():
     try:
-        Domain = input("""Enter To DomainExample >> "facebook.com" : """)
-        ip = socket.gethostbyname(Domain)
+        Domain = input("""Enter To Domain (Example >> "example.com") : """)
         page = f"https://{Domain}/robots.txt"
         req = requests.get(page).text  
+             
+        print(req)
         
-        print(f"Ip Address in Site Web " + str(ip))
+        print("Save File >> " + Domain + "_robots.txt")
         fa = open(f"{Domain}_robots.txt","w")
-        fa.write(ip + "\n" + req)
+        fa.write(req)
         fa.close 
+        
     except:
-        print(f"""Error Domain Example "facebook.com" >>> """ + str(Domain))
-        main()
+        print(f"""Error Domain Example "example.com" >>> """ + str(Domain))
+       
         
 main()
